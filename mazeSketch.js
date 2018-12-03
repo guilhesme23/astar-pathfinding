68e987171480e16ea3fdbac09e5693b266ab6f37
Guilherme Marques Rosa <marquesguilherme3@gmail.com>
2018-12-03 02:51:33 -0200

    Remove walls

--- mazeSketch.js
+++ mazeSketch.js
@@ -3,6 +3,7 @@ var w = 60
 var cols, rows
 var maze = []
 var current;
+var stack = []
 
 var pos = 0
 
@@ -29,22 +30,50 @@ function draw() {
   background(33)
   // pos = pos % maze.length
   // current = maze[pos]
-  
   current.visited = true
-  current.show()
+  for (let i in maze) {
+    maze[i].show();
+  }
+  
+  current.highlight()
   // for (let i in current.neighbors) {
   //   current.neighbors[i].show()
   // }
   let next = current.pickNeighbor()
   if (next) {
     next.visited = true
+    removeWall(current, next)
     current = next
   }
 
-  for (let i in maze) {
-    maze[i].show()
+  // pos++
+  frameRate(1)
+}
+
+function removeWall(first, second) {
+  let vertical = first.j - second.j
+  let horizontal = first.i - second.i
+
+  if (vertical === 1) {
+    // Second is the up neighbor
+    first.walls.top = false
+    second.walls.bottom = false
+    
+  } else if (vertical === -1) {
+    // Second is the bottom neighbor
+    first.walls.bottom = false
+    second.walls.top = false
+    
+  } else if (horizontal === 1) {
+    // Second is the left neighbor
+    first.walls.left = false
+    second.walls.right = false
+    
+  } else if (horizontal === -1) {
+    // Second is the right neighbor
+    first.walls.right = false
+    second.walls.left = false
+
   }
 
-  // pos++
-  frameRate(5)
 }
--- mazeSpot.js
+++ mazeSpot.js
@@ -8,7 +8,12 @@ class MazeCell {
     this.neighbors = []
 
     // Order: Top, Bottom, Left, Right
-    this.walls = [true, true, true, true]
+    this.walls = {
+      top: true,
+      bottom: true,
+      left: true,
+      right: true
+    }
   }
 
   index(i,j) {
@@ -55,6 +60,13 @@ class MazeCell {
     }
   }
 
+  highlight() {
+    let x = this.i * w
+    let y = this.j * w
+    fill(255,80,0)
+    rect(x,y,w,w)
+  }
+
   show() {
     let x = this.i * w
     let y = this.j * w
@@ -66,23 +78,23 @@ class MazeCell {
     }
 
     noFill()
-    stroke(255,0,0)
-    if (this.walls[0]) {
+    stroke(202)
+    if (this.walls.top) {
       // TOP
       line(x,y,x+w,y)
     }
 
-    if (this.walls[1]) {
+    if (this.walls.bottom) {
       // BOTTOM
       line(x,y+w,x+w,y+w)
     }
 
-    if (this.walls[2]) {
+    if (this.walls.left) {
       // LEFT
       line(x,y,x,y+w)
     }
 
-    if (this.walls[2]) {
+    if (this.walls.right) {
       // RIGHT
       line(x+w,y,x+w,y+w)
     }
